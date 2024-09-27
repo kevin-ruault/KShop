75c7077e8510afa26fca4687b96dca0da3352c6c
kevin-ruault <r_kevin@hotmail.fr>
2024-09-27 13:17:11 +0200

    router / axios products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import { createBrowserRouter, NavLink, Outlet, RouterProvider, useParams, useRouteError } from 'react-router-dom';
 import './App.scss';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <PageError />,
+    children: [
+      {
+        path: 'produit',
+        children: [
+          {
+            path: "",
+            element: <Products />
+          },
+          {
+            path: ":id",
+            element: <Single />
+          }
+        ]
+      },
+      {
+        path: 'contact',
+        element: <div>Contact</div>,
+      }
+    ]
+  }
+])
+
+
+function Products() {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/product").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  console.log(data)
+  return (
+    <ul>
+      {data.map((product) => (
+        <li key={product.id}>
+          <NavLink to={product.id}>{product.title}</NavLink>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function Single() {
+  const { id } = useParams()
+  return <>
+    <h1>Produit {id}</h1>
+  </>
+}
+
+function PageError() {
+  const error = useRouteError()
+  console.log(error)
+  return <>
+    <h1> Une erreur est survenue </h1>
+  </>
+}
+
+function Root() {
+  return <>
+    <header>
+      <nav>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/produit">Produit</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+    </header>
+    <div><Outlet /></div>
+  </>
+}
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
